9ca257ef1261840c379018432ce6964149f3dc94
yxs <problem_yxs@126.com>
2021-08-12 10:54:13 +0800

    code

--- 动态规划/712. 两个字符串的最小ASCII删除和.py
+++ 动态规划/712. 两个字符串的最小ASCII删除和.py
@@ -25,3 +25,30 @@
 链接：https://leetcode-cn.com/problems/minimum-ascii-delete-sum-for-two-strings
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        m, n = len(s1), len(s2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        # base case  // dp[m][n] = 0
+        for i in range(m-1, -1, -1):
+            dp[i][n] = dp[i+1][n] + ord(s1[i])
+        for j in range(n-1, -1, -1):
+            dp[m][j] = dp[m][j+1] + ord(s2[j])
+        # 状态转移
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if s1[i] == s2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = min(dp[i+1][j] + ord(s1[i]),
+                                   dp[i][j+1] + ord(s2[j]))
+        return dp[0][0]
+
+
+if __name__ == "__main__":
+    s1 = "delete"
+    s2 = "leet"
+    solution = Solution()
+    res = solution.minimumDeleteSum(s1, s2)
+    print(res)
